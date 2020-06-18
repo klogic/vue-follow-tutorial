f6f2f34ac890f16ba1b57463d5c08c8030a004aa
narongsak keawmanee <klogic@hotmail.co.th>
2020-06-18 23:57:56 +0700

    feat: 🎸 simple component system/ watch callback

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ var app7 = new Vue({
     ],
   },
 });
+
+// Define a new component called button-counter
+Vue.component("button-counter", {
+  data: function () {
+    return {
+      count: 0,
+    };
+  },
+  template:
+    '<button v-on:click="count++">You clicked me {{ count }} times.</button>',
+});
+
+new Vue({ el: "#components-demo" });
+
+var data = { a: 1 };
+var vm = new Vue({
+  el: "#example",
+  data: data,
+});
+vm.$watch("a", function (newValue, oldValue) {
+  console.log(oldValue + " has trigger to ", newValue);
+  // This callback will be called when `vm.a` changes
+});
