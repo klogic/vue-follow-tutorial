46a68e6c8740837a13aa334432ebcf350d8359d4
narongsak keawmanee <klogic@hotmail.co.th>
2020-06-23 22:21:27 +0700

    feat: 🎸 all form input

--- js/formInput.js
+++ js/formInput.js
@@ -4,3 +4,39 @@ new Vue({
     message: null,
   },
 });
+
+new Vue({
+  el: "#multiple-checkbox-example",
+  data: {
+    checkedNames: [],
+  },
+});
+
+new Vue({
+  el: "#radio-example",
+  data: {
+    picked: null,
+  },
+});
+new Vue({
+  el: "#value-binding-example",
+  data: {
+    selected: "",
+  },
+});
+
+Vue.component("custom-input", {
+  props: ["value"],
+  template: `
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  `,
+});
+new Vue({
+  el: "#value-binding-component",
+  data: {
+    searchText: "",
+  },
+});
