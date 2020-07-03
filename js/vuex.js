b7c20b8f244dd3c8ff448af55508a7f0ea428ae4
narongsak keawmanee <klogic@hotmail.co.th>
2020-07-03 21:52:12 +0700

    feat: 🎸 vue getter

--- js/vuex.js
+++ js/vuex.js
@@ -1,52 +1,89 @@
-// make sure to call Vue.use(Vuex) if using a module system
-const mapState = Vuex.mapState;
+// // make sure to call Vue.use(Vuex) if using a module system
+// const mapState = Vuex.mapState;
+// const store = new Vuex.Store({
+//   state: {
+//     count: 0,
+//   },
+//   mutations: {
+//     increment: (state) => state.count++,
+//     decrement: (state) => state.count--,
+//   },
+// });
+
+// const Counter = {
+//   template: `<div>
+//   </br>
+//   count: {{count}}
+//   </br>
+//   countAlias: {{countAlias}}
+//   </br>
+//   countPlusLocalState: {{ countPlusLocalState }}</div>`,
+//   data() {
+//     return { localCount: 3 };
+//   },
+//   computed: mapState({
+//     count: (state) => state.count,
+//     countAlias: "count",
+//     countPlusLocalState(state) {
+//       return state.count + this.localCount;
+//     },
+//   }),
+// };
+
+// new Vue({
+//   el: "#app",
+//   store,
+//   components: { Counter },
+//   template: `
+//     <div class="app">
+//       <counter></counter>
+//       <button @click="increment">+</button>
+//       <button @click="decrement">-</button>
+//     </div>
+//   `,
+//   methods: {
+//     increment() {
+//       store.commit("increment");
+//     },
+//     decrement() {
+//       store.commit("decrement");
+//     },
+//   },
+// });
+
 const store = new Vuex.Store({
   state: {
-    count: 0,
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+    ],
   },
-  mutations: {
-    increment: (state) => state.count++,
-    decrement: (state) => state.count--,
+  getters: {
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done);
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length;
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find((todo) => todo.id === id);
+    },
   },
 });
-
-const Counter = {
-  template: `<div>
-  </br>
-  count: {{count}}
-  </br>
-  countAlias: {{countAlias}}
-  </br>
-  countPlusLocalState: {{ countPlusLocalState }}</div>`,
-  data() {
-    return { localCount: 3 };
-  },
-  computed: mapState({
-    count: (state) => state.count,
-    countAlias: "count",
-    countPlusLocalState(state) {
-      return state.count + this.localCount;
-    },
-  }),
-};
-
 new Vue({
   el: "#app",
   store,
-  components: { Counter },
+  data: {},
   template: `
     <div class="app">
-      <counter></counter>
-      <button @click="increment">+</button>
-      <button @click="decrement">-</button>
+      doneTodosCount: {{doneTodosCount}}
     </div>
   `,
-  methods: {
-    increment() {
-      store.commit("increment");
-    },
-    decrement() {
-      store.commit("decrement");
+  computed: {
+    doneTodosCount() {
+      return this.$store.getters.doneTodosCount;
     },
   },
 });
+
+console.log("hi", store.getters.getTodoById(2));
